26b9a042392c920a93ff4e6f6c6d2a91d61e66bf
Kane Duong <dqkhanh.2k@gmail.com>
2023-01-13 11:32:26 +0700

    - update export file

--- src/index.js
+++ src/index.js
@@ -1,40 +1,84 @@
-export { CylinderGeometry } from "./geometry/CylinderGeometry.js";
-export { PlaneGeometry } from "./geometry/PlaneGeometry.js";
-export { CapsuleGeometry } from "./geometry/CapsuleGeometry.js";
-export { LatheGeometry } from "./geometry/LatheGeometry.js";
+import { CylinderGeometry } from "./geometry/CylinderGeometry.js";
+import { PlaneGeometry } from "./geometry/PlaneGeometry.js";
+import { CapsuleGeometry } from "./geometry/CapsuleGeometry.js";
+import { LatheGeometry } from "./geometry/LatheGeometry.js";
+import { AmbientLight } from "./light/AmbientLight.js";
+import { DirectionLight } from "./light/DirectionLight.js";
+import { Light } from "./light/Light.js";
+import { PointLight } from "./light/PointLight.js";
+import { BufferObject } from "./buffer/BufferObject.js";
+import { VertexBuffer } from "./buffer/VertexBuffer.js";
+import { IndexBuffer } from "./buffer/IndexBuffer.js";
+import { UniformBuffer } from "./buffer/UniformBuffer.js";
+import { Camera } from "./camera/Camera.js";
+import { PerspectiveCamera } from "./camera/PerspectiveCamera.js";
+import { OrbitControl } from "./camera/OrbitControl.js";
+import { GPUInstance, checkGPU } from "./core/GPUInstance.js";
+import { Renderer } from "./core/Renderer.js";
+import { Vector2 } from "./math/Vector2.js";
+import { Vector3 } from "./math/Vector3.js";
+import { Vector4 } from "./math/Vector4.js";
+import { Matrix3 } from "./math/Matrix3.js";
+import { Matrix4 } from "./math/Matrix4.js";
+import { Color } from "./math/Color.js";
+import { SceneNode } from "./objects/SceneNode.js";
+import { Mesh } from "./objects/Mesh.js";
+import { Skybox } from "./objects/Skybox.js";
+import { BufferGeometry } from "./geometry/BufferGeometry.js";
+import { CubeGeometry } from "./geometry/CubeGeometry.js";
+import { BoxGeometry } from "./geometry/BoxGeometry.js";
+import { SphereGeometry } from "./geometry/SphereGeometry.js";
+import { ShaderMaterial } from "./material/ShaderMaterial.js";
+import { NormalMaterial } from "./material/NormalMaterial.js";
+import { PhysicalMaterial } from "./material/PhysicalMaterial.js";
+import { SkyboxMaterial } from "./material/SkyboxMaterial.js";
+import { LambertMaterial } from "./material/LambertMaterial.js";
+import { WireFrameMaterial } from "./material/WireFrameMaterial.js";
+import { uuid } from "./utils/uuid.js";
+import { GLTFLoader } from "./loaders/GLTFLoader.js";
+import { TextureLoader } from "./loaders/TextureLoader.js";
 
-export { AmbientLight } from "./light/AmbientLight.js";
-export { DirectionLight } from "./light/DirectionLight.js";
-export { Light } from "./light/Light.js";
-export { PointLight } from "./light/PointLight.js";
+const KaneRender = {
+  CylinderGeometry,
+  PlaneGeometry,
+  CapsuleGeometry,
+  LatheGeometry,
+  AmbientLight,
+  DirectionLight,
+  Light,
+  PointLight,
+  BufferObject,
+  VertexBuffer,
+  IndexBuffer,
+  UniformBuffer,
+  Camera,
+  PerspectiveCamera,
+  OrbitControl,
+  GPUInstance,
+  checkGPU,
+  Renderer,
+  Vector2,
+  Vector3,
+  Vector4,
+  Matrix3,
+  Matrix4,
+  Color,
+  SceneNode,
+  Mesh,
+  Skybox,
+  BufferGeometry,
+  CubeGeometry,
+  BoxGeometry,
+  SphereGeometry,
+  ShaderMaterial,
+  NormalMaterial,
+  PhysicalMaterial,
+  SkyboxMaterial,
+  LambertMaterial,
+  WireFrameMaterial,
+  uuid,
+  GLTFLoader,
+  TextureLoader,
+};
 
-export { BufferObject } from "./buffer/BufferObject.js";
-export { VertexBuffer } from "./buffer/VertexBuffer.js";
-export { IndexBuffer } from "./buffer/IndexBuffer.js";
-export { UniformBuffer } from "./buffer/UniformBuffer.js";
-export { Camera } from "./camera/Camera.js";
-export { PerspectiveCamera } from "./camera/PerspectiveCamera.js";
-export { OrbitControl } from "./camera/OrbitControl.js";
-export { GPUInstance, checkGPU } from "./core/GPUInstance.js";
-export { Renderer } from "./core/Renderer.js";
-export { Vector2 } from "./math/Vector2.js";
-export { Vector3 } from "./math/Vector3.js";
-export { Vector4 } from "./math/Vector4.js";
-export { Matrix3 } from "./math/Matrix3.js";
-export { Matrix4 } from "./math/Matrix4.js";
-export { Color } from "./math/Color.js";
-export { SceneNode } from "./objects/SceneNode.js";
-export { Mesh } from "./objects/Mesh.js";
-export { Skybox } from "./objects/Skybox.js";
-export { BufferGeometry } from "./geometry/BufferGeometry.js";
-export { CubeGeometry } from "./geometry/CubeGeometry.js";
-export { BoxGeometry } from "./geometry/BoxGeometry.js";
-export { SphereGeometry } from "./geometry/SphereGeometry.js";
-export { ShaderMaterial } from "./material/ShaderMaterial.js";
-export { NormalMaterial } from "./material/NormalMaterial.js";
-export { PhysicalMaterial } from "./material/PhysicalMaterial.js";
-export { SkyboxMaterial } from "./material/SkyboxMaterial.js";
-export { LambertMaterial } from "./material/LambertMaterial.js";
-export { uuid } from "./utils/uuid.js";
-export { GLTFLoader } from "./loaders/GLTFLoader.js";
-export { TextureLoader } from "./loaders/TextureLoader.js";
+export default KaneRender;
